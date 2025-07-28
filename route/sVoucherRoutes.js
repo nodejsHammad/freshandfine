99e1da1b38b1acde0d5eabbd792c86708d22b005
nodejsHammad <nodejs.digitonz@gmail.com>
2025-07-29 00:09:17 +0500

    POS api add from strat

--- route/sVoucherRoutes.js
+++ route/sVoucherRoutes.js
@@ -4,9 +4,15 @@ import api_svoucher_control from "../controller/api_svoucher_control.js";
 import api_reporting_control from "../controller/api_reporting_control.js";
 import api_sales_report_control from "../controller/api_sales_report_control.js";
 import api_forecast_control from "../controller/api_forecast_control.js";
+import api_pos_control from "../controller/api_pos_control.js";
 
 sVoucherRoute.post("/getCustomerInvoice", api_reporting_control.getCustomerInvoice)
 sVoucherRoute.post("/getSalesReportByCustomer", api_sales_report_control.getSalesReportByCustomer)
 sVoucherRoute.post("/getSalesForecast", api_forecast_control.getSalesForecast)
+
+sVoucherRoute.post("/getData", api_pos_control.get_data)
+sVoucherRoute.post("/getPOS", api_pos_control.getPOS)
+
+
 
 export default sVoucherRoute
